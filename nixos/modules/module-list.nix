f0bb701af1d7529690ede2d2e40662db51a2f0c0
Parnell Springmeyer <parnell@digitalmentat.com>
2018-04-11 18:38:17 -0500

    Add gnome3 modules back in

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -118,6 +118,20 @@
   ./security/sudo.nix
   ./services/audio/alsa.nix
   ./services/desktops/geoclue2.nix
+  ./services/desktops/gnome3/at-spi2-core.nix
+  ./services/desktops/gnome3/evolution-data-server.nix
+  ./services/desktops/gnome3/gnome-disks.nix
+  ./services/desktops/gnome3/gnome-documents.nix
+  ./services/desktops/gnome3/gnome-keyring.nix
+  ./services/desktops/gnome3/gnome-online-accounts.nix
+  ./services/desktops/gnome3/gnome-online-miners.nix
+  ./services/desktops/gnome3/gnome-terminal-server.nix
+  ./services/desktops/gnome3/gnome-user-share.nix
+  ./services/desktops/gnome3/gpaste.nix
+  ./services/desktops/gnome3/gvfs.nix
+  ./services/desktops/gnome3/seahorse.nix
+  ./services/desktops/gnome3/sushi.nix
+  ./services/desktops/gnome3/tracker.nix
   ./services/editors/emacs.nix
   ./services/hardware/acpid.nix
   ./services/hardware/actkbd.nix
