736ba54e0f3dfeadc6496680ec195b8be1bf37e4
Andrew Childs <lorne@cons.org.nz>
2020-07-30 00:32:34 +0900

    compiler-rt: prevent platform sniffing on Darwin
    Exposed by the tbd stubs which contains fat libraries. The previously
    used proxy libraries were x86_64 only.

--- pkgs/development/compilers/llvm/10/compiler-rt.nix
+++ pkgs/development/compilers/llvm/10/compiler-rt.nix
@@ -33,6 +33,11 @@ stdenv.mkDerivation rec {
   ] ++ stdenv.lib.optionals (stdenv.hostPlatform.parsed.kernel.name == "none") [
     "-DCOMPILER_RT_BAREMETAL_BUILD=ON"
     "-DCOMPILER_RT_OS_DIR=baremetal"
+  ] ++ stdenv.lib.optionals (stdenv.hostPlatform.isDarwin) [
+    # The compiler-rt build infrastructure sniffs supported platforms on Darwin
+    # and finds i386;x86_64;x86_64h. We only build for x86_64, so linking fails
+    # when it tries to use libc++ and libc++api for i386.
+    "-DDARWIN_osx_ARCHS=${stdenv.hostPlatform.parsed.cpu.name}"
   ];
 
   outputs = [ "out" "dev" ];
