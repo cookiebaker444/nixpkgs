69d1771fb09f9704717bdc1a2fd2d66b93f3d48d
Gabriel Gonzalez <Gabriel439@gmail.com>
2021-05-18 10:42:25 -0700

    Patch through support for `specialArgs` (#9)
    This upstreams https://github.com/awakesecurity-dev/end-to-end/blob/d005f355c4b51cf39b54e34ecad89d98c6c7f279/awake-pkgs/nixpkgs/nixos-19_09.patch
    into our vendored Nixpkgs.  Doing so greatly simplifies the patch
    because we no longer need to thread through a `nixpkgs` argument.
    
    We also never needed to patch `./nixos/default.nix` as we were not
    using that file at all.

--- nixos/lib/testing.nix
+++ nixos/lib/testing.nix
@@ -5,9 +5,11 @@
   # Ignored
 , config ? {}
   # Modules to add to each VM
-, extraConfigurations ? [] }:
+, extraConfigurations ? []
+, specialArgs ? {}
+}:
 
-with import ./build-vms.nix { inherit system pkgs minimal extraConfigurations; };
+with import ./build-vms.nix { inherit system pkgs minimal extraConfigurations specialArgs; };
 with pkgs;
 
 let
